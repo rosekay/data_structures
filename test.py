e83f55fd44545929eaed7543f85a33b35b3d9f6e
Rose Kariuki <rosekariuki28@gmail.com>
2017-02-09 12:33:05 +0000

    add_event function

--- test.py
+++ test.py
@@ -11,12 +11,15 @@ user is able to:
 '''
 import calendar
 
+#create new calendar
+def title():
+	print '-' * 10
+	print "Calendar App."
+	 
 
-def create_cal():
+	
 
-	cal = calendar.month(2017, 1)
-	return cal
-
+#Get user choice 
 def get_user_choice():
 	print '-' * 10
 	print "Choose an Option Below:\n"
@@ -24,22 +27,37 @@ def get_user_choice():
 	print '[2] See All Events'
 	print '[3] View last Event\n'
 
-	choice = raw_input( "Enter your Choice. \n") 
+	
 
+#Add an event
 def add_event():
-	if choice == '1':
-		
-	
+	print '-' * 10
 
-	
-	
-	
+	choice = raw_input( "Enter your Choice. \n") 
+	if choice == '1':
+		mm = int(raw_input('Enter Month: \n'))
+		yy = int(raw_input('Enter Year: \n'))
+		dd = int(raw_input('Enter Day: \n'))
+
+		my_cal = calendar.month(yy, mm)
+		print my_cal
+
+	event = raw_input(" Add Event: \n")
+	events = event.title()
+
+	my_event = {dd: events}
+	print "On %s/%s/%s: %s " % (dd, mm, yy, events)
+		
+
+
 def see_all():
+	
 	pass
 def view_last_event():
 	pass			
 	
 
-print create_cal()
-print get_user_choice()
+# title()
+# get_user_choice()
+add_event()
 
