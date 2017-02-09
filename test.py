c83d081e0516169532dc49e542ff2f223ce44488
Rose Kariuki <rosekariuki28@gmail.com>
2017-02-09 13:16:03 +0000

    see_all and view_last_item functions

--- test.py
+++ test.py
@@ -10,54 +10,65 @@ user is able to:
 
 '''
 import calendar
-
-#create new calendar
-def title():
-	print '-' * 10
-	print "Calendar App."
-	 
-
-	
-
-#Get user choice 
-def get_user_choice():
-	print '-' * 10
-	print "Choose an Option Below:\n"
-	print '[1] Add an Event'
-	print '[2] See All Events'
-	print '[3] View last Event\n'
-
-	
-
-#Add an event
-def add_event():
-	print '-' * 10
-
-	choice = raw_input( "Enter your Choice. \n") 
-	if choice == '1':
-		mm = int(raw_input('Enter Month: \n'))
-		yy = int(raw_input('Enter Year: \n'))
-		dd = int(raw_input('Enter Day: \n'))
-
-		my_cal = calendar.month(yy, mm)
-		print my_cal
-
-	event = raw_input(" Add Event: \n")
-	events = event.title()
-
+class calendarApp():
+	events = ''
 	my_event = {dd: events}
-	print "On %s/%s/%s: %s " % (dd, mm, yy, events)
+	event_list = []
+
+	#Title message
+	def title():
+		print '-' * 10
+		print "Calendar App."
+		 
+
 		
 
+	#Get user choice 
+	def get_user_choice():
+		print '-' * 10
+		print "Choose an Option Below:\n"
+		print '[1] Add an Event'
+		print '[2] See All Events'
+		print '[3] View last Event\n'
 
-def see_all():
-	
-	pass
-def view_last_event():
-	pass			
-	
+		
 
-# title()
-# get_user_choice()
-add_event()
+	#Add an event
+	def add_event():
+		print '-' * 10
+
+		choice = raw_input( "Enter your Choice. \n") 
+		if choice == '1':
+			mm = int(raw_input('Enter Month: \n'))
+			yy = int(raw_input('Enter Year: \n'))
+			dd = int(raw_input('Enter Day: \n'))
+
+			my_cal = calendar.month(yy, mm)
+			print my_cal
+
+		event = raw_input(" Add Event: \n")
+		events = event.title()
+		
+		
+		print "On %s/%s/%s: %s " % (dd, mm, yy, events)
+
+
+	#See all events	
+	def see_all():
+		event_list = []
+		for event in my_event:
+			event_list.append(events)
+		print event_list
+
+		
+	#view last event	
+	def view_last_event():
+		return event_list[-1]			
+		
+
+	title()
+	get_user_choice()
+	add_event()
+	see_all()
+	view_last_event()
 
